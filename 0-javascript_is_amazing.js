4a890b6972b07839fffb98d78779469e4c0babd6
edward kwarteng owusu <108972325+Eddykwartofficial@users.noreply.github.com>
2025-07-12 18:17:48 +0000

    Update 0-javascript_is_amazing.js

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,5 +1,4 @@
-// Create a constant variable named myVar and assign it the specified string value.
-// The 'const' keyword ensures that the value of this variable cannot be changed later in the script.
+
 const myVar = "JavaScript is amazing";
 
 // Use console.log() to print the value of the myVar variable to the web console.
