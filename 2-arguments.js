75f2754899b194c5786a45c71b05a889839d1e72
edward kwarteng owusu <108972325+Eddykwartofficial@users.noreply.github.com>
2025-07-12 17:54:56 +0000

    Create 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,15 @@
+const argCount = ProcessingInstruction.argv.length - 2;
+
+if (argCount === 0) {
+
+    console.log("No argument");
+}
+
+else if (argCount ===1) {
+
+    console.log("Argument found");
+}
+
+else {
+    console.log("Argument found");
+}
